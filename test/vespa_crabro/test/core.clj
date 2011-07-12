f3ad2cc622a6e022f1512ff7fd615bfdbbfa1d8e
Kevin Downey <redchin@gmail.com>
2011-07-12 04:37:05 -0700

    test updates

--- test/vespa_crabro/test/core.clj
+++ test/vespa_crabro/test/core.clj
@@ -5,5 +5,13 @@
 (deftest test-vespa-crabro
   (with-open [server (create-server)
               mb (message-bus)]
-    (send-to mb "foo" {:x 1})
-    (receive-from mb "foo" (fn [x] (is (= x {:x 1}))))))
+    (testing "normal send/recieve"
+      (send-to mb "foo" {:x 1})
+      (receive-from mb "foo" (fn [x] (is (= x {:x 1})))))
+    (testing "send cookie recieve, re-send using new message bus"
+      (send-to mb "foo" (cookie mb))
+      (receive-from mb "foo"
+                    (fn [c]
+                      (with-open [mb2 (message-bus c)]
+                        (send-to mb2 "foo" {:x 2}))))
+      (receive-from mb "foo" (fn [x] (is (= x {:x 2})))))))
