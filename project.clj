bbd451fd79a7ece7b5115cbf2f8de3651f0c4956
Kevin Downey <redchin@gmail.com>
2011-08-11 11:30:54 -0700

    new repos update hornetq version

--- project.clj
+++ project.clj
@@ -1,10 +1,15 @@
 (defproject vespa-crabro "1.0.0-SNAPSHOT"
   :description "easy distribution for clojure"
-  :repositories {"jboss" "https://repository.jboss.org/nexus/content/repositories/releases/"}
+  :repositories {"jboss4" "http://repository.jboss.org/nexus/content/groups/public-jboss/"
+                 "hornetq" "http://hornetq.s3.amazonaws.com/"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.hornetq/hornetq-core "2.2.2.Final"]
-                 [org.hornetq/hornetq-core-client "2.2.2.Final"]
+                 [org.hornetq/hornetq-core "2.2.5.Final"]
+                 [org.hornetq/hornetq-core-client "2.2.5.Final"]
                  [org.hornetq/hornetq-transports "2.1.0.r9031"]
+                 [org.hornetq.rest/hornetq-rest "2.2.5.Final"]
                  [org.jboss.netty/netty "3.2.0.Final"]
+                 [org.jboss.resteasy/resteasy-jaxrs "1.2.1.GA"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [tjws/webserver "1.3.3"]
                  [commons-codec "1.5"]]
   :main vespa.server)
