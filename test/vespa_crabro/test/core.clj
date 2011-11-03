5474afe3fd4ba6ed052a660053465c33781895c6
Kevin Downey <redchin@gmail.com>
2011-11-02 23:31:19 -0700

    test changes

--- test/vespa_crabro/test/core.clj
+++ test/vespa_crabro/test/core.clj
@@ -3,41 +3,42 @@
         [vespa.protocols]
         [clojure.test]))
 
-;; (add-watch vespa.logging/log :stdout (fn [k r os obj] (println (map str obj))))
+;(add-watch vespa.logging/log :stdout (fn [k r os obj] (println (map str obj))))
 
 (deftest test-vespa-crabro
-  (with-open [server (create-server)
-              mb (message-bus)]
-    (testing "normal send/recieve"
-      (send-to mb "foo" {:x 1})
-      (receive-from mb "foo" (fn [x] (is (= x {:x 1})))))
-    (testing "send cookie recieve, re-send using new message bus"
-      (send-to mb "foo" (cookie mb))
-      (receive-from mb "foo"
-                    (fn [c]
-                      (with-open [mb2 (message-bus c)]
-                        (send-to mb2 "foo" {:x 2}))))
-      (receive-from mb "foo" (fn [x] (is (= x {:x 2})))))
-    (testing "broadcasting"
-      (with-open [mb1 (message-bus)
-                  mb2 (message-bus)]
-        (declare-broadcast mb1 "broadcast")
-        (declare-broadcast mb2 "broadcast")
-        (send-to mb1 "broadcast" {:x 3})
-        (receive-from mb1 "broadcast"
+  (binding [*in-vm-only* true]
+    (with-open [server (create-server)
+                mb (message-bus)]
+      (testing "normal send/recieve"
+        (send-to mb "foo" {:x 1})
+        (receive-from mb "foo" (fn [x] (is (= x {:x 1})))))
+      (testing "send cookie recieve, re-send using new message bus"
+        (send-to mb "foo" (cookie mb))
+        (receive-from mb "foo"
+                      (fn [c]
+                        (with-open [mb2 (message-bus c)]
+                          (send-to mb2 "foo" {:x 2}))))
+        (receive-from mb "foo" (fn [x] (is (= x {:x 2})))))
+      (testing "broadcasting"
+        (with-open [mb1 (message-bus)
+                    mb2 (message-bus)]
+          (declare-broadcast mb1 "broadcast")
+          (declare-broadcast mb2 "broadcast")
+          (send-to mb1 "broadcast" {:x 3})
+          (receive-from mb1 "broadcast"
+                        (fn [x]
+                          (is (= x {:x 3}))))
+          (receive-from mb2 "broadcast"
+                        (fn [x]
+                          (is (= x {:x 3}))))))
+      (testing "cloning"
+        (with-open [mb2 (.clone mb)]
+          (send-to mb2 "foo" {:x 1})
+          (is (nil? (receive-from mb "foo" (fn [x] (is (= x {:x 1})) nil))))))
+      (testing "expiration"
+        (send-to mb "foo" {:x 5} :expiration 1000)
+        (send-to mb "foo" {:x 6})
+        (Thread/sleep 2000)
+        (receive-from mb "foo"
                       (fn [x]
-                        (is (= x {:x 3}))))
-        (receive-from mb2 "broadcast"
-                      (fn [x]
-                        (is (= x {:x 3}))))))
-    (testing "cloning"
-      (with-open [mb2 (.clone mb)]
-        (send-to mb2 "foo" {:x 1})
-        (is (nil? (receive-from mb "foo" (fn [x] (is (= x {:x 1})) nil))))))
-    (testing "expiration"
-      (send-to mb "foo" {:x 5} :expiration 1000)
-      (send-to mb "foo" {:x 6})
-      (Thread/sleep 2000)
-      (receive-from mb "foo"
-                    (fn [x]
-                      (is (= x {:x 6})))))))
+                        (is (= x {:x 6}))))))))
